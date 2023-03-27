6f841c4c13e014fec5c406c8866283ca046b8c08
robsuarezc <rob7ny@gmail.com>
2023-03-28 00:23:49 +0200

    test

--- api/src/db.js
+++ api/src/db.js
@@ -30,9 +30,11 @@ sequelize.models = Object.fromEntries(capsEntries);
 
 // En sequelize.models están todos los modelos importados como propiedades
 // Para relacionarlos hacemos un destructuring
-const { Dog } = sequelize.models;
+const { Dog,Temperament } = sequelize.models;
 
 // Aca vendrian las relaciones
+Dog.belongsToMany(Temperament, { through: 'dog_temperament' });
+Temperament.belongsToMany(Dog, { through: 'dog_temperament' });
 // Product.hasMany(Reviews);
 
 module.exports = {
--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -5,6 +5,17 @@ const { Router } = require('express');
 
 const router = Router();
 
+// Importar controladores
+const dogsController = require('../controllers/dogsController');
+const temperamentsController = require('../controllers/temperamentsController');
+
+// Rutas
+router.get('/dogs', dogsController.getDogs);
+router.get('/dogs/:idRaza', dogsController.getDogById);
+router.get('/dogs/name', dogsController.getDogByName);
+router.post('/dogs', dogsController.createDog);
+router.get('/temperaments', temperamentsController.getTemperaments);
+
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
 
