92fd4c859175a4234dfbf872da4332d2d5ae9fe3
robsuarezc <rob7ny@gmail.com>
2023-03-28 00:22:52 +0200

    1

--- api/src/models/Dog.js
+++ api/src/models/Dog.js
@@ -4,9 +4,32 @@ const { DataTypes } = require('sequelize');
 module.exports = (sequelize) => {
   // defino el modelo
   sequelize.define('dog', {
+    id: {
+      type: DataTypes.UUID,
+      primaryKey: true,
+      autoIncrement: true,
+      allowNull: false,
+    },
+    image: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
     name: {
       type: DataTypes.STRING,
       allowNull: false,
     },
+    height: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    weight: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    lifespan: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
   });
+  return Dog
 };
